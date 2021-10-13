946f2e9c34a30252953fb4a6ccbe9086c304445c
ezzowali <79482255+ezzowali@users.noreply.github.com>
2021-10-13 16:08:25 +0300

    Add files via upload

---
+++ models/Users.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+
+var smav2Schema =new Schema({
+
+
+  firstName_En:String,
+  middleName_En:String,
+  lastName_En:String,
+  email:{ type: String, required: true, unique: true },
+  phone:Number,
+  gender:String,
+  age:Number,
+  city:Array,
+  university:String,
+  EducationLevel:Array,
+  Specialty:Array,
+  SCFHS:String,
+  times:String,
+  accept:String,
+  url_video:String,
+
+ 
+  
+
+});
+
+
+
+
+
+module.exports = mongoose.model('Users', smav2Schema);
